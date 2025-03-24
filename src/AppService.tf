197fdb3284cf33973977b670a6a5f8bdd4a9c4c5
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-03-23 23:48:03 -0500

    Move enable_telemetry for consistency

--- src/AppService.tf
+++ src/AppService.tf
@@ -1,12 +1,11 @@
 module "app_rg" {
-  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
-  version = "~>0.2.1"
+  source           = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version          = "~>0.2.1"
+  enable_telemetry = var.enable_telemetry
 
   name     = replace(local.naming_structure, "{resourceType}", "rg-app")
   location = var.location
   tags     = var.tags
-
-  enable_telemetry = var.enable_telemetry
 }
 
 module "asp" {
