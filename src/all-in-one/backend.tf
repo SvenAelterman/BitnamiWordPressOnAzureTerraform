f12d8f16ee84370f9f4fd48cba0419dbe1aa75ed
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-04-24 12:17:33 -0500

    Delete backend.tf files from repo again

--- src/all-in-one/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = "tfstate"
-    key                  = "all_in_one.tfstate"
-    use_azuread_auth     = true
-  }
-}
--- src/split-deployment/Foundation/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = "tfstate"
-    key                  = "foundation.tfstate"
-    use_azuread_auth     = true
-  }
-}
