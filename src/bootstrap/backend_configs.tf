891556922950d1e0a635eafd05bb1508f5a01965
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-04-24 10:16:06 -0500

    Bootstrap code to create tfstate storage account and output backend.tf files

---
+++ src/bootstrap/backend_configs.tf
@@ -0,0 +1,18 @@
+resource "local_file" "all_in_one_backend_configs" {
+  for_each = local.backend_config_folders
+
+  content  = <<EOF
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "${var.resource_group_name}"
+        storage_account_name = "${module.storage.name}"
+        container_name       = "${split("/", module.storage.containers.tfstate.id)[12]}"
+        key                  = "${each.key}.tfstate"
+        use_azuread_auth     = true
+    }
+}
+  EOF
+  filename = "${each.value}/backend.tf"
+}
+
+# At the time of writing, the storage account module output does not include the container name, so we parse it from the ID.
